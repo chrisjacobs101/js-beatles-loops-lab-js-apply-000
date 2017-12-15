14437710477e6cedfa4f6c789cede396b4ccc905
chrisjacobs101 <chrisjacobs101@gmail.com>
2017-12-15 20:53:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function theBeatlesPlay (musicians, instruments) {
+  
+  var outputArray = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    
+    outputArray.push
+    
+  }
+  
+}
+
+//"John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
